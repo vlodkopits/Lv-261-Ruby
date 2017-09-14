74446dfe0e9b5dfe661d38d503a6261f59fd2986
vlodkopits <vlodkopits@gmail.com>
2017-09-14 21:09:10 +0300

    final fix , rubocopcheck

--- obolonka.rb
+++ obolonka.rb
@@ -1,57 +1,51 @@
 require './abramove.rb'
 require 'json'
 include Abramove
-
+# simple graphic interface forAbramove tasks
 class Obolonka
-  def get_json
+  def getjson
     file = open('tasks.json').read
-    parsed = JSON.parse(file)["tasks"]
+    JSON.parse(file)['tasks']
   end
 
-	def get_tasks_desc  
-	  get_json.each do |task| 
-	    puts "#{task['id']}. #{task['desc']}" 
-	  end
-	end
+  def gettasks_desc
+    getjson.each do |task|
+      puts "#{task['id']}. #{task['desc']}"
+    end
+  end
 
-	def get_task_id(id)	  
-	  task_id = []
+  def gettask_id(id)
+    task_id = []
 
-	  get_json.each do |task| 
-	    task_id << task['id'] if task['id'].to_i == id
-	  end
+    getjson.each do |task|
+      task_id << task['id'] if task['id'].to_i == id
+    end
 
-	  task_id.first
-	end
+    task_id.first
+  end
 
-	def get_task_method(id) 
-	  task_method = []
+  def gettask_method(id)
+    task_method = []
 
-	  get_json.each do |task| 
-	    task_method << task['method'] if task['id'].to_i == id
-	  end
+    getjson.each do |task|
+      task_method << task['method'] if task['id'].to_i == id
+    end
 
-	  task_method.first
-	end
+    task_method.first
+  end
 
-	def start
-		get_tasks_desc		
+  def start
+    gettasks_desc
 
-		print 'Input task number:'
-		n = gets.to_i
-		task_id = get_task_id(n)
+    print 'Input task number:'
+    n = gets.to_i
+    task_id = gettask_id(n)
+    n == task_id ? send(gettask_method(n)) : (print 'Wrong task number!')
 
-
-		if n == task_id
-			send(get_task_method(n))
-		else
-			print "Wrong task number!"
-		end
-		print 'Another task? y/n: '
+    print 'Another task? y/n: '
     gets.chomp == 'y' ? start : return
-	end
+  end
 end
 
 run = Obolonka.new
 run.start
-
